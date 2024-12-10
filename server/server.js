b28c33f292ec02f336031a875111c0467ceae8b7
omesh choudhary <omeshparswal@gmail.com>
2024-12-10 14:16:36 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -3,12 +3,18 @@ import dotenv from "dotenv"
 import ConnectDB from "./DB/db.js"
 import Router from "./routes/user.routes.js"
 import ClassRouter from "./routes/class.route.js"
+import cors from "cors"
 dotenv.config({
     path:"./.env"
 })
 let PORT=process.env.PORT
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin:"*",
+    methods:"HEAD, POST, GET, PUT, PATCH",
+    credentials:true,
+}))
 app.use("/api/user",Router)
 app.use("/api/data",ClassRouter)
 
