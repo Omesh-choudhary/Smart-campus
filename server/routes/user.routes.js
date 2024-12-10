cf5043273fda423d343d120db47a171ae095e744
omesh choudhary <omeshparswal@gmail.com>
2024-12-10 14:07:23 +0530

    added

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { login, registration, UserData } from "../controllers/user.controllers.js"
+import { ClassData, login, registration, UserData } from "../controllers/user.controllers.js"
 import tokenMiddleware from "../middlewares/auth.middleware.js"
 
 const Router = express.Router()
